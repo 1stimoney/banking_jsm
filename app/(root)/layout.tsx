7753ae413d15ad22a3ba9200e5182cd510c93967
1stimoney <daudasheriff020@gmail.com>
2025-01-03 22:55:31 -0800

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
+import { redirect, useRouter } from "next/navigation";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const loggeIn = await getLoggedInUser();
+
+  if (!loggeIn) redirect("/sign-in");
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggeIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggeIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
+}
