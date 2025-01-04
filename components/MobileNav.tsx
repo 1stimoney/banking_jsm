35393a88c46511723881c84b7b760043bbb5d2ba
1stimoney <daudasheriff020@gmail.com>
2025-01-04 01:48:32 -0800

    plaid

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -15,6 +15,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Footer from "./Footer";
+import PlaidLink from "./PlaidLink";
 
 const MobileNav = ({ user }: MobileNavProps) => {
   const pathname = usePathname();
@@ -91,7 +92,8 @@ const MobileNav = ({ user }: MobileNavProps) => {
                     </SheetClose>
                   );
                 })}
-                USER
+
+                <PlaidLink user={user} />
               </nav>
             </SheetClose>
             <div className="mb-7 pb-16">
