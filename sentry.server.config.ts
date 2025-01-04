796233e424e67ac24d2f605d64d198a12006aeec
1stimoney <daudasheriff020@gmail.com>
2025-01-03 23:31:37 -0800

    added global error

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: "https://3038d805df17b0ee0e7daa396c9af584@o4508505399492608.ingest.us.sentry.io/4508505405063168",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
