222f34423ec10d9ac2b9fc98b2f3c21d73c0d20d
1stimoney <daudasheriff020@gmail.com>
2025-01-03 23:28:38 -0800

    add dynamic

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -8,12 +8,11 @@ Sentry.init({
   dsn: "https://3038d805df17b0ee0e7daa396c9af584@o4508505399492608.ingest.us.sentry.io/4508505405063168",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: process.env.NODE_ENV === 'development',
-  
+  spotlight: process.env.NODE_ENV === "development",
 });
